9914161cc61e6cbc6704ca335ee3b0b0d5916322
maheetchoudhary@gmail.com <maheetchoudhary@MAHEETs-MacBook-Air.local>
2026-06-06 10:45:32 +0530

    day 3 assignment

---
+++ day 3/print_prime_number_in_range.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int start, end, i, j, flag;
+    printf("enter start and end ofn range:");
+    scanf("%d %d", &start, &end);
+    printf("prime number are:\n");
+    for (i = start; i <= end; i++)
+    {
+        if (i <= 1)
+        continue;
+        flag = 1;
+        for (j =2; j <= i/2; j++)
+        {
+            if (i % j == 0)
+            {
+                flag = 0;
+                break;
+
+            }
+            
+        }
+        if(flag)
+        printf("%d", i);
+        
+    }
+    return 0;
+}
