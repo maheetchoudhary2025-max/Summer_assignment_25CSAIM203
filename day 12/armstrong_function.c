b61879556855c87eb9323a27e6f3653540f89a5f
maheetchoudhary@gmail.com <maheetchoudhary@MAHEETs-MacBook-Air.local>
2026-06-15 21:04:54 +0530

    day 12 assignment

---
+++ day 12/armstrong_function.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int armstrong(int n)
+{
+    int temp = n, sum = 0, rem;
+    while (n > 0)
+    {
+        rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+
+    return (temp == sum);
+}
+   int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (armstrong(num))
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
+}
