c9e96b1b9d697855dd2f2d8a948be9e998807b0d
maheetchoudhary@gmail.com <maheetchoudhary@MAHEETs-MacBook-Air.local>
2026-06-25 17:53:55 +0530

    day 22 assignment

---
+++ day 22/count_words.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() 
+{
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) 
+    {
+        if (str[i] == ' ' && str[i + 1] != ' ')
+            words++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
